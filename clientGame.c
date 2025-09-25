ee85ca138d27df0866ebea084c4af8433fa64d27
Alexzame23 <alejazam@ucm.es>
2025-09-25 18:20:10 +0200

    prueba

---
+++ clientGame.c
@@ -0,0 +1,152 @@
+#include "clientGame.h"
+
+void sendMsgToServer(int socket, char* message){
+	int l = strlen(message);
+	if(send(socket,&l,sizeof(l),0) < 0)
+		showError("Error al enviar la longitud del mensaje");
+	if(send(socket,message,l,0) < 0)
+		showError("Error al enviar el mensaje");
+}
+
+unsigned int receiveUi (int socket){
+	unsigned int l;
+	if(recv(socket,&l,sizeof(l),0) <= 0)
+		showError("Error al recibir el code");
+	return l;
+}
+
+void receiveMsg(int socket, char *message){
+
+	int l;
+	memset(message,0,STRING_LENGTH);
+	if(recv(socket, &l, sizeof(l), 0) <= 0)
+		showError("Error al recibir la longitud del mensaje");
+	if(recv(socket, message, l, 0) <= 0)
+		showError("Error al recibir el mensaje");
+}
+
+unsigned int readBet (){
+
+	int isValid, bet=0;
+	tString enteredMove;
+ 
+		// While player does not enter a correct bet...
+		do{
+
+			// Init...
+			bzero (enteredMove, STRING_LENGTH);
+			isValid = TRUE;
+
+			printf ("Enter a value:");
+			fgets(enteredMove, STRING_LENGTH-1, stdin);
+			enteredMove[strlen(enteredMove)-1] = 0;
+
+			// Check if each character is a digit
+			for (int i=0; i<strlen(enteredMove) && isValid; i++)
+				if (!isdigit(enteredMove[i]))
+					isValid = FALSE;
+
+			// Entered move is not a number
+			if (!isValid)
+				printf ("Entered value is not correct. It must be a number greater than 0\n");
+			else
+				bet = atoi (enteredMove);
+
+		}while (!isValid);
+
+		printf ("\n");
+
+	return ((unsigned int) bet);
+}
+
+unsigned int readOption (){
+
+	unsigned int bet;
+
+		do{		
+			printf ("What is your move? Press %d to hit a card and %d to stand\n", TURN_PLAY_HIT, TURN_PLAY_STAND);
+			bet = readBet();
+			if ((bet != TURN_PLAY_HIT) && (bet != TURN_PLAY_STAND))
+				printf ("Wrong option!\n");			
+		} while ((bet != TURN_PLAY_HIT) && (bet != TURN_PLAY_STAND));
+
+	return bet;
+}
+
+int main(int argc, char *argv[]){
+
+	int socketfd;						/** Socket descriptor */
+	unsigned int port;					/** Port number (server) */
+	struct sockaddr_in server_address;	/** Server address structure */
+	char* serverIP;						/** Server IP */
+	unsigned int endOfGame;				/** Flag to control the end of the game */
+	tString playerName;					/** Name of the player */
+	unsigned int code;					/** Code */
+
+
+	// Check arguments!
+	if (argc != 3){
+		fprintf(stderr,"ERROR wrong number of arguments\n");
+		fprintf(stderr,"Usage:\n$>%s serverIP port\n", argv[0]);
+		exit(0);
+	}
+
+	// Get the server address
+	serverIP = argv[1];
+
+	// Get the port
+	port = atoi(argv[2]);
+
+	socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(socketfd < 0){
+		showError("Error while creating the socket");
+	}
+
+	serverIP = argv[1];
+
+	memset(&server_address, 0, sizeof(server_address));
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(serverIP);
+	server_address.sin_port = htons(port);
+
+	if(connect(socketfd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+		showError("Error while stablishing connection");
+	}
+
+	do{
+		memset(playerName, 0, STRING_LENGTH);
+		printf("Enter player name:");
+		fgets(playerName, STRING_LENGTH-1, stdin);
+	}while(strlen(playerName) <= 2);
+
+	sendMsgToServer(socketfd, playerName);
+
+	endOfGame = FALSE;
+
+	printf("Creating the play room\n\n");
+
+	receiveMsg(socketfd, playerName);
+	printf("You are playing againts %s\n\n", playerName);
+	printf("Game starts!\n\n");
+	unsigned int stack;
+	do{
+		code = receiveUi(socketfd);
+		if(code == TURN_BET){
+			receiveUi(socketfd);
+		}
+		else if(code == TURN_PLAY_WAIT){
+
+		}
+		else{
+			
+		}
+
+	}while(!endOfGame);
+
+	if(close(socketfd) == -1){
+		showError("Error while closing the client socket");
+	}
+		
+}
---
+++ serverGame.c
@@ -0,0 +1,234 @@
+#include "serverGame.h"
+#include <pthread.h>
+
+void sendMsgToPlayer(int socketClient, char *message){	
+
+	int l = strlen(message);
+	if(send(socketClient, &l, sizeof(l), 0) < 0)
+		showError("Error al enviar la longitud del mensaje");
+	if(send(socketClient, message, l, 0) < 0)
+		showError("Error al enviar el mensaje");
+}
+
+void receiveMsg(int socketClient, char *message){
+
+	int l;
+	memset(message,0,STRING_LENGTH);
+	if(recv(socketClient, &l, sizeof(l), 0) <= 0)
+		showError("Error al recibir la longitud del mensaje");
+	if(recv(socketClient, message, l, 0) <= 0)
+		showError("Error al recibir el mensaje");
+}
+
+void sendUi(int socketClient, unsigned int code)
+{
+	if(send(socketClient, &code, sizeof(code), 0) < 0)
+		showError("Error al enviar el code");
+}
+
+tPlayer getNextPlayer (tPlayer currentPlayer){
+
+	tPlayer next;
+
+		if (currentPlayer == player1)
+			next = player2;
+		else
+			next = player1;
+
+	return next;
+}
+
+void initDeck (tDeck *deck){
+
+	deck->numCards = DECK_SIZE; 
+
+	for (int i=0; i<DECK_SIZE; i++){
+		deck->cards[i] = i;
+	}
+}
+
+void clearDeck (tDeck *deck){
+
+	// Set number of cards
+	deck->numCards = 0;
+
+	for (int i=0; i<DECK_SIZE; i++){
+		deck->cards[i] = UNSET_CARD;
+	}
+}
+
+void printSession (tSession *session){
+
+		printf ("\n ------ Session state ------\n");
+
+		// Player 1
+		printf ("%s [bet:%d; %d chips] Deck:", session->player1Name, session->player1Bet, session->player1Stack);
+		printDeck (&(session->player1Deck));
+
+		// Player 2
+		printf ("%s [bet:%d; %d chips] Deck:", session->player2Name, session->player2Bet, session->player2Stack);
+		printDeck (&(session->player2Deck));
+
+		// Current game deck
+		if (DEBUG_PRINT_GAMEDECK){
+			printf ("Game deck: ");
+			printDeck (&(session->gameDeck));
+		}
+}
+
+void initSession (tSession *session){
+
+	clearDeck (&(session->player1Deck));
+	session->player1Bet = 0;
+	session->player1Stack = INITIAL_STACK;
+
+	clearDeck (&(session->player2Deck));
+	session->player2Bet = 0;
+	session->player2Stack = INITIAL_STACK;
+
+	initDeck (&(session->gameDeck));
+}
+
+unsigned int calculatePoints (tDeck *deck){
+
+	unsigned int points;
+
+		// Init...
+		points = 0;
+
+		for (int i=0; i<deck->numCards; i++){
+
+			if (deck->cards[i] % SUIT_SIZE < 9)
+				points += (deck->cards[i] % SUIT_SIZE) + 1;
+			else
+				points += FIGURE_VALUE;
+		}
+
+	return points;
+}
+
+unsigned int getRandomCard (tDeck* deck){
+
+	unsigned int card, cardIndex, i;
+
+		// Get a random card
+		cardIndex = rand() % deck->numCards;
+		card = deck->cards[cardIndex];
+
+		// Remove the gap
+		for (i=cardIndex; i<deck->numCards-1; i++)
+			deck->cards[i] = deck->cards[i+1];
+
+		// Update the number of cards in the deck
+		deck->numCards--;
+		deck->cards[deck->numCards] = UNSET_CARD;
+
+	return card;
+}
+
+
+void *gameThread(void* args){
+	tThreadArgs* threadArgs = (tThreadArgs*) args;
+	int socket1 = threadArgs->socketPlayer1;
+	int socket2 = threadArgs->socketPlayer2;
+	free(args);
+	tSession sesion;
+	unsigned int endOfGame = FALSE;	
+
+	receiveMsg(socket1, sesion.player1Name);
+	receiveMsg(socket2, sesion.player2Name);
+
+	printf("Initialising a new game with the players %s and %s", sesion.player1Name, sesion.player2Name);
+
+	sendMsgToPlayer(socket1, sesion.player2Name);
+	sendMsgToPlayer(socket2, sesion.player1Name);
+
+	initSession(&sesion);
+	et1, TURN_BET);
+	et1, sesion.player1Stack);
+	et2, TURN_PLAY_WAIT);
+	do{
+
+	}while(!endOfGame);
+	
+}
+
+
+
+int main(int argc, char *argv[]){
+
+	int socketfd;						/** Socket descriptor */
+	struct sockaddr_in serverAddress;	/** Server address structure */
+	unsigned int port;					/** Listening port */
+	struct sockaddr_in player1Address;	/** Client address structure for player 1 */
+	struct sockaddr_in player2Address;	/** Client address structure for player 2 */
+	int socketPlayer1;					/** Socket descriptor for player 1 */
+	int socketPlayer2;					/** Socket descriptor for player 2 */
+	unsigned int clientLength;			/** Length of client structure */
+	tThreadArgs *threadArgs; 			/** Thread parameters */
+	pthread_t threadID;					/** Thread ID */
+
+
+	// Seed
+	srand(time(0));
+
+	// Check arguments
+	if (argc != 2) {
+		fprintf(stderr,"ERROR wrong number of arguments\n");
+		fprintf(stderr,"Usage:\n$>%s port\n", argv[0]);
+		exit(1);
+	}
+
+	socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);	//Creamos el descriptor del socket
+
+	if(socketfd < 0)										//Comprobamos que se creo correctamente
+		showError("Error while opening socket");
+
+	memset(&serverAddress, 0, sizeof(serverAddress));		//Iniciamos vacia la estructura del servidor
+
+	port = atoi(argv[1]);
+
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port = htons(port);
+
+	if(bind(socketfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+		showError("Error while binding");
+	}
+
+	if(listen(socketfd, 150) < 0){
+		showError("Error while listening");
+	}
+
+	clientLength = sizeof(struct sockaddr_in);
+
+	while(1){
+
+		socketPlayer1 = accept(socketfd, (struct sockaddr *)&player1Address, &clientLength);
+		if(socketPlayer1 < 0){
+			showError("Error while opening socket");
+		}
+
+		socketPlayer2 = accept(socketfd, (struct sockaddr *)&player2Address, &clientLength);
+		if(socketPlayer2 < 0){
+			showError("Error while opening socket");
+		}
+
+		threadArgs = (tThreadArgs*)malloc(sizeof(tThreadArgs));
+        if (threadArgs == NULL) {
+            showError("Error while saving memory for the thread");
+        }
+
+		threadArgs->socketPlayer1 = socketPlayer1;
+		threadArgs->socketPlayer2 = socketPlayer2;
+
+		if(pthread_create(&threadID, NULL, gameThread, (void*)threadArgs) != 0){
+			showError("Error creating the game thread");
+		}
+
+		pthread_detach(threadID);
+			
+	}	
+
+	return 0;	
+}
